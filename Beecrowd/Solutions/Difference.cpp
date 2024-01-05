5fd65017cc1748fb621d9fe25149fc697bfc9620
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-05 02:00:00 +0000

    Add `Difference.cpp`

---
+++ Beecrowd/Solutions/Difference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, Difference;
+    cin >> A >> B >> C >> D;
+    Difference = (A * B - C * D);
+    cout << "DIFERENCA = " << Difference << endl;
+    return 0;
+}
