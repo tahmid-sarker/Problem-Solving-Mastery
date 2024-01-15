645424599f42938759b0739345c8feb9d0a18874
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-15 11:30:00 +0000

    Add `Highest and Position.cpp`

---
+++ Beecrowd/Solutions/Highest and Position.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, max = 0, pos = 0;
+    for(int i = 1; i <= 100; i++)
+    {
+        cin >> n;
+        if(n > max)
+        {
+            max = n;
+            pos = i;
+        }
+    }
+    cout << max << endl;
+    cout << pos << endl;
+    return 0;
+}
