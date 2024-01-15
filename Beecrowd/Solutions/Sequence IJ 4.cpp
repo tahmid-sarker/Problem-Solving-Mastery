574bde5ab6eb5de95ec2880325b9da55b8fcf350
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-15 11:30:00 +0000

    Add `Sequence IJ 4.cpp`

---
+++ Beecrowd/Solutions/Sequence IJ 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(float i = 0; i <= 2.2; i += 0.2)
+    {
+        for(float j = 1; j <= 3.0; j++)
+        {
+            cout << "I=" << i << " " << "J=" << i+j << endl;
+        }
+    }
+    return 0;
+}
