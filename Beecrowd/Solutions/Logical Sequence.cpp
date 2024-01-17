d1926fab0203a935c223862aadd57a8241e387bb
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-17 02:30:00 +0000

    Add `Logical Sequence.cpp`

---
+++ Beecrowd/Solutions/Logical Sequence.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; i++)
+    {
+        cout << i << " " << i * i << " " << i * i * i << endl;
+        cout << i << " " << i * i + 1 << " " << i * i * i + 1 << endl;
+    }
+    return 0;
+}
