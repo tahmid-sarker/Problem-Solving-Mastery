cbaa94821a91a58a312fb1d3722c5cd474598b15
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-20 05:30:00 +0000

    Add `Line in Array.cpp`

---
+++ Beecrowd/Solutions/Line in Array.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int L;
+    char T;
+    float M[12][12], sum = 0.0;
+    while(cin >> L >> T)
+    {
+        for(int i = 0; i < 12; i++)
+        {
+            for(int j = 0; j < 12; j++)
+            {
+                cin >> M[i][j];
+                if(i == L)
+                {
+                    sum += M[i][j];
+                }
+            }
+        }
+        if(T == 'S')
+        {
+            cout << fixed << setprecision(1) << sum << endl;
+        }
+        else if(T == 'M')
+        {
+            cout << fixed << setprecision(1) << sum / 12.0 << endl;
+        }
+    }
+    return 0;
+}
