1917eec31c2769d11861c44c83df9d467aa74ebe
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-22 07:30:00 +0000

    Add `Inferior Area.cpp`

---
+++ Beecrowd/Solutions/Inferior Area.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    char O;
+    double M[12][12], sum = 0.0;
+    while(cin >> O)
+    {
+        for(int i = 0; i < 12; i++)
+        {
+            for(int j = 0; j < 12; j++)
+            {
+                cin >> M[i][j];
+                if(j > (11 - i) && j < i)
+                {
+                    sum += M[i][j];
+                }
+            }
+        }
+        if(O == 'S')
+        {
+            cout << fixed << setprecision(1) << sum << endl;
+        }
+        else if(O == 'M')
+        {
+            cout << fixed << setprecision(1) << sum / 30.0 << endl;
+        }
+    }
+    return 0;
+}
