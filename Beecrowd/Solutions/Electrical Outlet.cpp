5a2db33820067eb56c467c7071f731a78e4d303d
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-25 10:30:00 +0000

    Add `Electrical Outlet.cpp`

---
+++ Beecrowd/Solutions/Electrical Outlet.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T1, T2, T3, T4;
+    cin >> T1 >> T2 >> T3 >> T4;
+    int calculate = (T1 + T2 + T3 + T4) - 3;
+    cout << calculate << endl;
+    return 0;
+}
