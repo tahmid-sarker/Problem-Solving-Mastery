a3a710ab2d1d9152c951cd6c4e4301f9433afd79
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-29 03:15:00 +0000

    Add `I am Toorg!.cpp`

---
+++ Beecrowd/Solutions/I am Toorg!.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    string str;
+    while(N--)
+    {
+        cin >> str;
+        cout << "I am Toorg!" << endl;
+    }
+    return 0;
+}
