7b3bdbb81a0b91237053de19cbca29dd1777198a
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-03 02:00:00 +0000

    Add `Balloon of Honor.cpp`

---
+++ Beecrowd/Solutions/Balloon of Honor.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cin >> ch;
+    if(ch == 'A')
+        cout << "1" << endl;
+    else if(ch == 'B')
+        cout << "2" << endl;
+    else if(ch == 'C')
+        cout << "3" << endl;
+    else if(ch == 'D')
+        cout << "4" << endl;
+    else if(ch == 'E')
+        cout << "5" << endl;
+    else if(ch == 'F')
+        cout << "6" << endl;
+    else if(ch == 'G')
+        cout << "7" << endl;
+    else if(ch == 'H')
+        cout << "8" << endl;
+    else if(ch == 'I')
+        cout << "9" << endl;
+    else if(ch == 'J')
+        cout << "10" << endl;
+    else if(ch == 'K')
+        cout << "11" << endl;
+    else if(ch == 'L')
+        cout << "12" << endl;
+    else if(ch == 'M')
+        cout << "13" << endl;
+    else if(ch == 'N')
+        cout << "14" << endl;
+    else if(ch == 'O')
+        cout << "15" << endl;
+    else if(ch == 'P')
+        cout << "16" << endl;
+    else if(ch == 'Q')
+        cout << "17" << endl;
+    else if(ch == 'R')
+        cout << "18" << endl;
+    else if(ch == 'S')
+        cout << "19" << endl;
+    else if(ch == 'T')
+        cout << "20" << endl;
+    else if(ch == 'U')
+        cout << "21" << endl;
+    else if(ch == 'V')
+        cout << "22" << endl;
+    else if(ch == 'W')
+        cout << "23" << endl;
+    else if(ch == 'X')
+        cout << "24" << endl;
+    else if(ch == 'Y')
+        cout << "25" << endl;
+    else if(ch == 'Z')
+        cout << "26" << endl;
+    else
+        cout << "Invalid Input" << endl;
+    return 0;
+}
