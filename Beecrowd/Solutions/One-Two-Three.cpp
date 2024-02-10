6e81da75456ce81dc862964a20120dbe618f5c1b
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-10 06:00:00 +0000

    Add `One-Two-Three.cpp`

---
+++ Beecrowd/Solutions/One-Two-Three.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        string str;
+        cin >> str;
+        if(str.length() == 5)
+        {
+            cout << "3" << endl;
+        }
+        else if((str[0] == 'o' && str[1] == 'n') || (str[1] == 'n' && str[2] == 'e') || (str[0] == 'o' && str[2] == 'e'))
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            cout << "2" << endl;
+        }
+    }
+    return 0;
+}
