d82a94530f7f84422a5cae1bd766ea0ce79c5292
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-17 03:30:00 +0000

    Add `Trinomial Triangle.cpp`

---
+++ Beecrowd/Solutions/Trinomial Triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int R;
+    cin >> R;
+    long long int sum = pow(3, R);
+    cout << sum << endl;
+    return 0;
+}
