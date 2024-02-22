df9efa1cbb97dc96ee596f049cee96fe24f460b7
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-22 01:00:00 +0000

    Add `Positives and Average.cpp`

---
+++ Beecrowd/Solutions/Positives and Average.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    float num, sum = 0;
+    int i, count = 0;
+    for(i = 1; i <= 6; i++)
+    {
+        cin >> num;
+        if(num > 0)
+        {
+          sum = sum + num;
+          count++;
+        }
+    }
+    float average = sum / count;
+    cout << count << " valores positivos" << endl;
+    cout << fixed;
+    cout << setprecision(1) << average << endl;
+    return 0;
+}
