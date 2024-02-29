909939c88b0b748aea453ec638e9a05415bece84
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-29 06:30:00 +0000

    Add `Count Items Matching a Rule.cpp`

---
+++ LeetCode/Solutions/Count Items Matching a Rule.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int count = 0;
+        int size = items.size();
+        if (ruleKey == "type") {
+            for (int i = 0; i < size; i++) {
+                if (items[i][0] == ruleValue)
+                    count++;
+            }
+        }
+        if (ruleKey == "color") {
+            for (int i = 0; i < size; i++) {
+                if (items[i][1] == ruleValue)
+                    count++;
+            }
+        }
+        if (ruleKey == "name") {
+            for (int i = 0; i < size; i++) {
+                if (items[i][2] == ruleValue)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
